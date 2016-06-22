30ecb463db7da44c3d4aca0f87bc78c3875c9ea3
lasun21 <lasun21@gmail.com>
2016-06-22 15:46:05 +0900

    [third commint]

--- app/src/main/java/com/example/administrator/testproject/MainActivity.java
+++ app/src/main/java/com/example/administrator/testproject/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView title = (TextView)findViewById(R.id.txt_title);
+        title.setText("First World");
 
         Button btn = (Button)findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
